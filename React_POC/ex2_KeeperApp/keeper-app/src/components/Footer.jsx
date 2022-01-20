169147c5dcf76e79f5b1ec2695127aa3e84f96e6
rishcodelib <ri.sbh23@gmail.com>
2022-01-20 22:41:03 +0530

    working on keeper app

---
+++ React_POC/ex2_KeeperApp/keeper-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const d = new Date().getFullYear();
+
+function Footer() {
+  return (<footer>
+    <p>Copyright ⓒ {d}</p>
+  </footer>
+  )
+}
+
+export default Footer;
