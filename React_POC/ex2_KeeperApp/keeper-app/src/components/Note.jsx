beb41e504dc304810876c87d03761d58e4b7eff2
rishcodelib <ri.sbh23@gmail.com>
2022-01-21 00:12:52 +0530

    prop driling

--- React_POC/ex2_KeeperApp/keeper-app/src/components/Note.jsx
+++ React_POC/ex2_KeeperApp/keeper-app/src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 function Note() {
-  return <div className="note">
+  return (<div className="note">
     <h1> This is a Note</h1>
     <p> This is the Content</p>
-  </div>
+  </div>)
 }
 
 export default Note;
