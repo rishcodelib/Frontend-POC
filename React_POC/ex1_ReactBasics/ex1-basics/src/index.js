e3d11823534464e95580b929714e4cd78b33add5
rishcodelib <ri.sbh23@gmail.com>
2022-01-20 19:19:48 +0530

    keeperApp init React

--- React_POC/ex1_ReactBasics/ex1-basics/src/index.js
+++ React_POC/ex1_ReactBasics/ex1-basics/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Headings from './Headings';
+import pi, { add, div, mul, sub } from './math'
 
 ReactDOM.render(
   <div>
     <Headings />
-
+    <ul>
+      <li> A Calculator Page</li>
+      <li>Value of PI =  {pi}</li>
+      <li>Addition of 1 and 2  = {add(1, 2)}</li>
+      <li>Subtraction of 1 and 2 = {sub(1, 2)}</li>
+      <li>Multiplication of 1 and 2 = {mul(1, 2)}</li>
+      <li>Division of 1 and 2 = {div(1, 2)}</li>
+    </ul>
   </div>,
 
   document.getElementById('root')
---
+++ React_POC/ex1_ReactBasics/ex1-basics/src/math.jsx
@@ -0,0 +1,23 @@
+const pi = 3.1415962;
+
+function add(a, b) {
+  return a + b;
+}
+
+
+function sub(a, b) {
+  return a - b;
+}
+function mul(a, b) {
+  return a * b;
+}
+function div(a, b) {
+  return a / b;
+}
+
+
+
+export default pi;
+export {
+  add, sub, mul, div
+}
