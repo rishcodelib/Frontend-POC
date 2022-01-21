f2d1ef55273c290e4774e96f2dbeae8dc1d4ce5d
rishcodelib <ri.sbh23@gmail.com>
2022-01-22 01:13:39 +0530

    funciton op update

---
+++ React_POC/ex5_map_filter_reduce/ex5/Find.js
@@ -0,0 +1,18 @@
+var numbers = [3, 56, 2, 48, 5];
+console.log(numbers);
+
+
+//Find: Find the first Item that matches from an array.
+
+const newNbr = numbers.find(function (num) {
+  return num > 10;
+})
+
+// this will not loop through entire array like FILTER.
+//it will stop as soon as it finds it's first Item
+console.log(newNbr);
+
+
+//OP
+// [ 3, 56, 2, 48, 5 ]
+// 56
--- React_POC/ex5_map_filter_reduce/ex5/FindIndex.js
+++ React_POC/ex5_map_filter_reduce/ex5/FindIndex.js
@@ -1,1 +1,21 @@
+var numbers = [3, 56, 2, 48, 5];
+console.log(numbers);
+
+
 //findindex: find the index of the first item that matches.
+
+
+// Similar to Find() funciton, instead of finding the number, it finds the index of first item that matches
+
+
+const newNbr = numbers.findIndex(function (num) {
+  return num > 10;
+})
+
+
+console.log(newNbr);
+
+
+// Op
+//[ 3, 56, 2, 48, 5 ]
+// 1
