05b820d647077d8d057241279a078d2c7fa9602b
rishcodelib <ri.sbh23@gmail.com>
2022-01-30 23:51:33 +0530

    working on react forms

--- React_POC/ex12_react_forms/react-forms/src/reactForms.js
+++ React_POC/ex12_react_forms/react-forms/src/reactForms.js
@@ -5,7 +5,7 @@ function ReactForms() {
   // const [fName, setfName] = useState("")
   // const [LName, setLname] = useState("")
 
-   // function updateFname(event) {
+  // function updateFname(event) {
   //   const firstName = event.target.value;
   //   setfName(firstName);
   // }
@@ -16,25 +16,51 @@ function ReactForms() {
   // }
 
 
-   // we are setting fullName to a new object
-   const [fullName, setFullName] = useState({
+  // we are setting fullName to a new object
+  const [fullName, setFullName] = useState({
     fName: "",
     lName: ""
   })
-  
-  function handleChange(event) {
-    const newVal = event.target.value; //input value= ""
-    const inputName = event.target.name; // taking fome the input type = "" 
 
-    if (inputName === "fName") {
-      setFullName({ fName: newVal })
-    }
-    else if (inputName === 'lName') {
-      setFullName({ fName: newVal });
-    }
-    console.log(newVal);
-    console.log(inputName);
+  function handleChange(event) {
+    // const newVal = event.target.value; //input value= ""
+    // const inputName = event.target.name; // taking fome the input type = "" 
+
+    //or 
+    //try this and change inputName to name and newVal to value
+    const { value, name } = event.target
+
+
+    setFullName(prevValue => {
+
+      if (name === "fname") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        }
+      }
+      else if (name === 'lname') {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        }
+      }
+
+    })
+
+    // if (inputName === "fname") {
+    //   return setFullName({ fName: newVal })
+    // }
+    // else if (inputName === 'lname') {
+    //   setFullName({ fName: newVal });
+    // }
+
+
+    // console.log(newVal);
+    // console.log(inputName);
+
   }
+
 
 
   return (<div>
@@ -42,8 +68,10 @@ function ReactForms() {
       {/* <h1> Hello {fName} {LName}</h1> */}
       <h1> Hello {fullName.fName} {fullName.lName}</h1>
       <form>
+
         {/* <input name="fname" onChange={updateFname} value={fName} placeholder='FirstName' />
         <input name="fname" onChange={updateLname} value={LName} placeholder='LastName' /> */}
+
         <input name="fname" onChange={handleChange}
           // value={fullName.fName} 
           placeholder='FirstName' />
